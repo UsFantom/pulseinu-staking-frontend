ebae25e427b2a6fe8f4b05c4aff741fdb31ea871
Vilayhong Bounthanh <31374816+Edison4mobile@users.noreply.github.com>
2023-10-05 21:08:01 +0700

    fix
    - add link to logo, whitepaper, telegram, twitter
    - add image to token

--- src/queries/useTokenMutation.js
+++ src/queries/useTokenMutation.js
@@ -1,15 +1,18 @@
+import { getConfig } from '../config';
 import { useERC20Contract } from '../hooks/useContract';
 import { useStakingToken } from './useStaking';
 import { useMutation } from 'react-query';
 
 export const useStakingTokenMutation = () => {
   const stakingToken = useStakingToken();
+  const config = getConfig();
+  const stakingTokenImage = config?.contracts?.stakingTokenImage;
   const tokenContract = useERC20Contract(stakingToken.data, false);
 
   return useMutation('stakingTokenMutation', async () => {
     const symbol = await tokenContract.symbol();
     const decimals = await tokenContract.decimals();
 
-    return { symbol, decimals, address: stakingToken.data };
+    return { symbol, decimals, address: stakingToken.data, image: stakingTokenImage };
   });
 };
